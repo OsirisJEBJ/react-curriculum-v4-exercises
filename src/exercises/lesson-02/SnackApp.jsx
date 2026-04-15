4ac82b2feb47092d43d0b79ec77e94a782f2ac4b
OsirisJEBJ <estradasanchezjao@gmail.com>
2026-04-14 22:01:40 -0700

    Exercise Lesson02 Week 2 REACT

---
+++ src/exercises/lesson-02/SnackApp.jsx
@@ -0,0 +1,14 @@
+import SnackFooter from './SnackFooter';
+import SnackList from './SnackList';
+import SnackHeader from './SnackHeader';
+
+function SnackApp() {
+  return (
+    <div>
+      <SnackHeader />
+      <SnackList />
+      <SnackFooter />
+    </div>
+  );
+}
+export default SnackApp;
