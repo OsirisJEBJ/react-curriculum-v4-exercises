950fb76ea0d1648facedb1900314165e8f3dbc83
OsirisJEBJ <estradasanchezjao@gmail.com>
2026-04-28 22:44:20 -0700

    useRef/useState/events/handlers

--- src/exercises/lesson-04/FindCorrectHook.jsx
+++ src/exercises/lesson-04/FindCorrectHook.jsx
@@ -1,16 +1,23 @@
 // TOPIC: Choose the correct tool: useRef vs useState
 // TASK: Make sure it updates the text *without* triggering a re-render
+import { useRef } from 'react';
+
 export default function FindCorrectHook() {
-  let clickCount = 0; // ← incorrect implementation
+  const clickCount = useRef(0);
+  const displayRef = useRef(null);
 
   function handleClick() {
-    clickCount++;
+    clickCount.current++;
+    displayRef.current.textContent = `${clickCount.current} Clicks`;
   }
 
   return (
     <div>
       <h2>useRef vs useState Decision</h2>
-      <button onClick={handleClick}>{clickCount} Clicks</button>
+
+      <button ref={displayRef} onClick={handleClick}>
+        0 Clicks
+      </button>
     </div>
   );
 }
